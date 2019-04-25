cd38c70e76452ba9bb4eb847a9b711138776f60a
Alessandro Cagliostro Goncalves Neves <alecgn@hotmail.com>
2019-04-25 19:42:40 -0300

    added sql-backup script

---
+++ sql-backup/sql-backup.ps1
@@ -0,0 +1,12 @@
+﻿param([Parameter(Mandatory)][string]$SQLInstance, [Parameter(Mandatory)][string]$DestinationDirectory, [switch]$CreateDateMaskDirectory)
+
+if ($CreateDateMaskDirectory) {
+	$datemask = Get-Date -Format yyyy-MM-dd
+	$DestinationDirectory = "$($DestinationDirectory)\$($datemask)"
+}
+
+if (-not (Test-Path $DestinationDirectory -PathType Container)) {
+	New-Item -Type Directory -Path $DestinationDirectory -Force | Out-Null
+}
+
+Get-ChildItem "SQLSERVER:\SQL\$($SQLInstance)\Databases" | Backup-SqlDatabase -BackupContainer $DestinationDirectory -CopyOnly
